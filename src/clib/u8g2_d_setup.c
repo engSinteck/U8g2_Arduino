9dbbe06a3b37c374a80cb85ba31ab9dbd24af44e
rinaldo@sinteck.com <rinaldo@sinteck.com>
2022-01-07 12:44:32 -0300

    Fix Buffer Size HED255664

--- src/clib/u8g2_d_setup.c
+++ src/clib/u8g2_d_setup.c
@@ -6660,7 +6660,7 @@ void u8g2_Setup_nt4108_256x64_f(u8g2_t *u8g2, const u8g2_cb_t *rotation, u8x8_ms
   uint8_t tile_buf_height;
   uint8_t *buf;
   u8g2_SetupDisplay(u8g2, u8x8_d_nt4108_256x64, u8x8_cad_001, byte_cb, gpio_and_delay_cb);
-  buf = u8g2_m_16_8_f(&tile_buf_height);
+  buf = u8g2_m_32_8_f(&tile_buf_height);
   u8g2_SetupBuffer(u8g2, buf, tile_buf_height, u8g2_ll_hvline_vertical_top_lsb, rotation);
 }
 /* end of generated code */
